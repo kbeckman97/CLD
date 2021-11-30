c6a334ad21db978cec915919e3aa3c7a7e603da2
kbeckman97 <92381569+kbeckman97@users.noreply.github.com>
2021-11-30 21:46:11 +0100

    bugs beheben

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import botocore.exceptions
+import numpy as np
+import skimage.measure
 import streamlit as st
 import boto3
 from PIL import Image
 from datetime import datetime
 import time
 import os
-
 currentTime = None
 
 
@@ -15,54 +16,55 @@ currentTime = None
 s3 = boto3.resource(
     service_name='s3',
     region_name='eu-central-1',
-    aws_access_key_id="AccesskeyID",
-    aws_secret_access_key="Secretaccesskey"
+    aws_access_key_id=st.secrets["AccesskeyID"],
+    aws_secret_access_key=st.secrets["Secretaccesskey"]
 )
+
 
 def load_image(img):
     im = Image.open(img)
-    #image = np.array(im)
     return im
 
 
 def main():
-    global currentTime
+
+    for file in os.listdir("images"):
+        os.remove("images/" + file)
+
     uploadFile = st.file_uploader(label="Upload files here", type=[ 'png'])
     if uploadFile is not None:
         # Perform your Manupilations (In my Case applying Filters)
-        img = load_image(uploadFile)
-        st.image(img)
+        img_upload = load_image(uploadFile)
+        st.image(img_upload)
         st.write("Image Uploaded Successfully")
         if st.button("Save and upload"):
 
             now = datetime.now()
             currentTime = now.strftime("%d-%m-%Y-%H-%M-%S")
             imageNameAWS_upload = "Test_Webseite/" + currentTime + ".PNG"
-            imageNameStre_upload = currentTime + ".PNG"
+            imageNameStre_upload = "images/" + currentTime + ".PNG"
 
-            img.save(imageNameStre_upload)
+            img_upload.save(imageNameStre_upload)
             s3.Bucket('mybucket-tes-3').upload_file(Filename=imageNameStre_upload, Key=imageNameAWS_upload)
-            os.remove(imageNameStre_upload)
+
             with st.spinner("Waiting"):
                 time.sleep(45)
-            imageNameStre_download = currentTime + "Download.PNG"
+            imageNameStre_download = "images/" + currentTime + "Download.PNG"
             imageNameAWS_download = "processed_images/" + currentTime + ".PNG"
             try:
                 s3.Bucket('mybucket-tes-3').download_file(Key=imageNameAWS_download, Filename=imageNameStre_download)
             except botocore.exceptions.ClientError:
                 s3.Bucket('mybucket-tes-3').download_file(Key=imageNameAWS_download, Filename=imageNameStre_download)
 
-            img = Image.open(imageNameStre_download)
-            st.image(img)
-
             print("Download Sucessfull")
             with open(imageNameStre_download, "rb") as file:
                 btn = st.download_button(
                 label = "Download image with bounding boxes",
                 data = file,
-                file_name = "Strahlen.png",
+                file_name = "images/" + "Strahlen.png",
                 mime = "image/png"
                 )
+
     else:
         st.write("Make sure you image is in PNG Format.")
 
@@ -71,20 +73,32 @@ def main():
         imgConvert = load_image(convertImage)
         st.image(imgConvert)
         st.write("Image Uploaded Successfully")
+
+        now = datetime.now()
+        currentTime = now.strftime("%d-%m-%Y-%H-%M-%S")
+        imageNameGray = "images/" + currentTime + ".png"
         if st.button("Convert to grayscale"):
-            imgGray = imgConvert.convert('1')
-            imgGray.save('test_gray.png')
+            imgGray = imgConvert.convert('L')
+            #size = [1280, 720]
+            #imgGray = imgGray.resize(size)
+            imgGray = np.array(imgGray)
+            imgGray = skimage.measure.block_reduce(imgGray, (3,3), np.max)
+            imgGray = Image.fromarray(imgGray)
+            imgGray.save(imageNameGray)
             st.image(imgGray)
 
-            with open("test_gray.png", "rb") as file:
+            with open(imageNameGray, "rb") as file:
                 btn = st.download_button(
                 label = "Download grayscale image",
                 data = file,
-                file_name = "test_gray.png",
+                file_name = currentTime + ".png",
                 mime = "image/png"
                 )
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+
+
